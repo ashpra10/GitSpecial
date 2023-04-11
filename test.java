880a06f6cff2325438a8a46842ccf074bef9cbde
Ashutosh Pradhan <ashnsoul822@gmail.com>
2023-04-11 21:50:58 +0530

    this is the first version

---
+++ test.java
@@ -0,0 +1,18 @@
+class test
+{
+	int age;
+	String name;
+	test(int age, String name)
+	{
+		this.age= age;
+		this.name= name;
+	}
+	void display(){
+		System.out.println("The age of "+ this.name + " is "+this.age +"." );
+	}
+	public static void main(String [] args)
+	{
+		test obj= new test(50, "Biranchi Narayan Pradhan");
+		System.out.println("ashpra");
+	}
+}
